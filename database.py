81bf5b718ec101ea3f37d262961ab5c4fcd3699f
Alfio Martini <alfio.martini@gmail.com>
2020-06-29 19:38:27 -0300

    Updated database.py

--- database.py
+++ database.py
@@ -3,6 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 import os 
 
-# set up database s
+# set up database 
+# The PostgreSQL dialect uses psycopg2 as the default DBAPI
 engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
